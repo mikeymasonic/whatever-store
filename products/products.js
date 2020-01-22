7d44f5ff156b4ed0681415de057b4fd08f38b736
Michael Romay <amerror@gmail.com>
2020-01-21 17:43:27 -0800

    added default css and fixed logic in products.js

--- products/products.js
+++ products/products.js
@@ -1,10 +1,10 @@
 import gear from '../data/inventory-data.js';
 import renderProducts from './render-products.js';
 
-const list = document.getElementById('fruits');
+const list = document.getElementById('gear');
 
 for (let i = 0; i < gear.length; i++) {
-    const gear = gear[i];
-    const dom = renderProducts(gear);
+    const gearArray = gear[i];
+    const dom = renderProducts(gearArray);
     list.appendChild(dom);
 }
