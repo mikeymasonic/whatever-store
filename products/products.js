cb167a2cff4441e9ae02a2c0d46acab2e9acde84
Michael Romay <amerror@gmail.com>
2020-01-21 17:29:48 -0800

    added logic to render-products.js

--- products/products.js
+++ products/products.js
@@ -1,10 +1,10 @@
 import gear from '../data/inventory-data.js';
-import renderGear from './render-products.js';
+import renderProducts from './render-products.js';
 
 const list = document.getElementById('fruits');
 
 for (let i = 0; i < gear.length; i++) {
     const gear = gear[i];
-    const dom = renderGear(gear);
+    const dom = renderProducts(gear);
     list.appendChild(dom);
 }
--- products/render-products.js
+++ products/render-products.js
@@ -1,1 +1,31 @@
-export gear
+function renderProducts(gear) {
+    const li = document.createElement('li');
+    li.className = gear.category;
+    li.title = gear.description;
+
+    const h3 = document.createElement('h3');
+    h3.textContent = gear.name;
+    li.appendChild(h3);
+
+    const img = document.createElement('img');
+    img.src = '../assets/' + gear.image;
+    img.alt = gear.name + ' image';
+    li.appendChild(img);
+
+    const p = document.createElement('p');
+    p.className = 'price';
+
+    const usd = '$' + gear.price.toFixed(2);
+    p.textContent = usd;
+
+    const button = document.createElement('button');
+    button.textContent = 'Add';
+    button.value = gear.code;
+    p.appendChild(button);
+
+    li.appendChild(p);
+
+    return li;
+}
+
+export default renderProducts;
