4c6056bb281c7ed716918af60c83dd9dc8828f84
Michael Romay <amerror@gmail.com>
2020-01-23 11:15:55 -0800

    cleaning up

--- shopping-cart/shopping-cart.js
+++ shopping-cart/shopping-cart.js
@@ -16,23 +16,3 @@ for (let i = 0; i < cart.length; i++) {
 
 const orderTotal = calcOrderTotal(cart, gear);
 orderTotalCell.textContent = toUSD(orderTotal);
-
-
-// import cart from '../data/cart.js';
-// import fruits from '../data/fruits.js';
-// import { findById, calcOrderTotal, toUSD } from '../common/utils.js';
-// import renderLineItem from './render-line-item.js';
-
-// const tbody = document.querySelector('tbody');
-// const orderTotalCell = document.getElementById('order-total-cell');
-
-// for (let i = 0; i < cart.length; i++) {
-//     const lineItem = cart[i];
-//     const fruit = findById(fruits, lineItem.id);
-//     const dom = renderLineItem(lineItem, fruit);
-
-//     tbody.appendChild(dom);
-// }
-
-// const orderTotal = calcOrderTotal(cart, fruits);
-// orderTotalCell.textContent = toUSD(orderTotal);
