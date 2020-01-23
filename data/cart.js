08dea0a23d9a00e664ff6da121a1a6a3a26c9d63
Michael Romay <amerror@gmail.com>
2020-01-22 17:08:47 -0800

    added data to cart.js

---
+++ data/cart.js
@@ -0,0 +1,24 @@
+const cart = [{
+    id: 'midi',
+    quantity: 2
+}, {
+    id: 'expressed-joy',
+    quantity: 5
+}, {
+    id: 'bitcrush-pedal',
+    quantity: 1
+}, {
+    id: 'midi-to-dmx',
+    quantity: 10
+}, {
+    id: 'cdj-to-midi',
+    quantity: 3
+}, {
+    id: 'midi-to-osc',
+    quantity: 1
+}, {
+    id: 'multi-effects-pedal',
+    quantity: 4
+}];
+
+export default cart;
